5f29206c002ae6e934fdb363866f80f938eac463
Marco Galindo <marco.gamaliel@gmail.com>
2022-10-07 18:35:43 -0300

    add security with bodyparser & use compress for big responses

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import { createKoaServer } from 'routing-controllers'
 import koaHelmet from 'koa-helmet'
 import nocache from 'koajs-nocache'
+import bodyParser from 'koa-bodyparser'
+import compress from 'koa-compress'
 import { CorsMiddleware } from './application/middlewares/cors/cors.middleware'
 import { CorrelationIdMiddleware } from './application/middlewares/correlation-id/correlation-id.middleware'
 import { ErrorHandlerMiddleware } from './application/middlewares/error-handler/error-hanler.middleware'
@@ -25,8 +27,10 @@ const app = createKoaServer({
 app.init = (): void => {
   const port = config.get<number>('PORT') || 3000
   app.listen(port)
-  app.use(koaHelmet)
-  app.use(nocache)
+  app.use(koaHelmet())
+  app.use(nocache())
+  app.use(bodyParser())
+  app.use(compress())
   logger.info(`Server listening on port ${port}`)
 }
 
